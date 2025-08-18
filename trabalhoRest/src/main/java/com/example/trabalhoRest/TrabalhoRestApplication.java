a69e357910074a8461799bd081391b68f292b91c
Luis Gustavo <luisgustavo.mac13@gmail.com>
2025-08-18 11:13:52 -0300

    Commit Inicial

---
+++ trabalhoRest/src/main/java/com/example/trabalhoRest/TrabalhoRestApplication.java
@@ -0,0 +1,13 @@
+package com.example.trabalhoRest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabalhoRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrabalhoRestApplication.class, args);
+	}
+
+}
